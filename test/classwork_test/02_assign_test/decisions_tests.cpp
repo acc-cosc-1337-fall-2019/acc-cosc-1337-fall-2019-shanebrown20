deb62f5f670bffdcf47a6d99d2b7dc8b679b71ee
Shane Brown <shane.brown1@g.austincc.edu>
2019-09-04 19:40:29 -0500

    updated travis CI file

--- test/classwork_test/02_assign_test/decisions_tests.cpp
+++ test/classwork_test/02_assign_test/decisions_tests.cpp
@@ -8,8 +8,8 @@ TEST_CASE("Verify Test Configuration", "verification") {
 
 TEST_CASE("Test gross pay function")
 {
-	REQUIRE(gross_pay(10,10) == 100);
-	REQUIRE(gross_pay(40,10) == 400);
+	REQUIRE(gross_pay(10,15) == 150);
+	REQUIRE(gross_pay(5,20) == 100);
 	REQUIRE(gross_pay(50,10) == 550);
 
 }
