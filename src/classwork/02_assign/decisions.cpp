7eb4b5b310125f445f38c66b1866411c97f6d2d3
Shane Brown <shane.brown1@g.austincc.edu>
2019-09-04 19:24:20 -0500

    examples for if statements and classwork 02

--- src/classwork/02_assign/decisions.cpp
+++ src/classwork/02_assign/decisions.cpp
@@ -1,1 +1,14 @@
+#include "decisions.h"
 //Write the function code that returns the product of hours and hourly_rate.
+
+double gross_pay(double hours, double hourly_rate)
+{
+	double overtime_hours = 0;
+	double overtime_rate = 1.5 * hourly_rate;
+	if (hours > 40)
+	{
+		overtime_hours = hours - 40;
+		hours = 40;
+	}
+	return hours * hourly_rate + overtime_hours * overtime_rate;
+}
