565c0cd995cffec0212b137f3f8473496b4e2ec2
Shane Brown <shane.brown1@g.austincc.edu>
2019-09-11 16:30:03 -0500

    Fixed include file for tests and added more test cases

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "decisions.h"
+#include "../../../homework/02_decisions/decisions.h"
 
 
 TEST_CASE("Verify Test Configuration", "verification") {
@@ -10,10 +10,14 @@ TEST_CASE("Verify Test Configuration", "verification") {
 TEST_CASE("Test get_grade_points function")
 {
 	REQUIRE(get_grade_points("A") == 4);
-	REQUIRE(get_grade_points("X") == -1);
+	REQUIRE(get_grade_points("B") == 3);
+	REQUIRE(get_grade_points("C") == 2);
+	REQUIRE(get_grade_points("D") == 1);
+	REQUIRE(get_grade_points("F") == 0);
 }
 TEST_CASE("Test calculate_gpa function")
 {
-	REQUIRE(calculate_gpa(27,9) == 3.0);
-	REQUIRE(calculate_gpa(42,12) == 3.5);
+	REQUIRE(calculate_gpa(9, 27) == 3.0);
+	REQUIRE(calculate_gpa(12, 42) == 3.5);
+	REQUIRE(calculate_gpa(120, 390) == 3.25);
 }
