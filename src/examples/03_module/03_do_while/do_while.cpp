cb765052f7c8655a3ed54614925bfb8e10e266cf
Shane Brown <shane.brown1@g.austincc.edu>
2019-09-16 19:28:50 -0500

    loops examples

--- src/examples/03_module/03_do_while/do_while.cpp
+++ src/examples/03_module/03_do_while/do_while.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include "do_while.h"
-#include "switch.h"
+
 
 using std::cout;
 using std::cin;
@@ -8,7 +8,36 @@ using std::cin;
 //Write code for void function prompt_user to loop until
 //user opts not to continue.  
 
+void prompt_user()
+{
+	auto user_choice = 'y';
 
+	do
+	{
+		// some more code for program
+		cout << "Loop again y or n? ";
+		cin >> user_choice;
+	}
+	while (user_choice == 'y' || user_choice == 'Y');
+}
+
+string menu(int menu_option)
+{
+	switch (menu_option)
+	{
+	case 1:
+		return "Option 1";
+	case 2:
+		return "Option 2";
+	case 3:
+		return "Option 3";
+	case 4:
+		return "Option 4";
+	default:
+		return "Invalid";
+	}
+	return string();
+}
 //Write code for function run_menu that prompts  user for a 
 //number from 1 to 4 and displays the option user selected.
 /*
@@ -17,3 +46,27 @@ folder.
 
 */
 
+
+
+void run_menu()
+{
+	auto user_choice = 'y';
+	auto choice = 0;
+	do
+	{
+		cout << "Enter menu option: ";
+		cin >> choice;
+
+		while (choice < 1 || choice > 4)
+		{
+			cout << "Enter menu option: ";
+			cin >> choice;
+		}
+		cout << menu(choice) << "\n";
+
+		cout << "Continue y or n";
+		cin >> user_choice;
+
+
+	} while(user_choice == 'y' || user_choice == 'Y');
+}
