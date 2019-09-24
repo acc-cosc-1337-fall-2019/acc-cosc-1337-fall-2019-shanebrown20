457f86682b0aec830a227391046cd40a8e1365cd
Shane Brown <shane.brown1@g.austincc.edu>
2019-09-23 19:04:45 -0500

    vector examples

--- src/examples/03_module/06_vectors/vec.cpp
+++ src/examples/03_module/06_vectors/vec.cpp
@@ -1,6 +1,7 @@
 #include "for_ranged.h"
 #include<iostream>
 #include<vector>
+#include "vec.h"
 
 using std::vector;
 
@@ -10,7 +11,16 @@ The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
 */
 
+void loop_vector_w_index()
+{
+	vector<int> nums = { 3,5,99,8 };
 
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		std::cout << nums[i] << "\n";
+	}
+
+}
 
 /*
 Write code for function loop_vector_w_index with a vector of int pass by value parameter.
@@ -18,14 +28,26 @@ The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
 */
 
-
+void loop_vector_w_index(std::vector<int> nums)
+{
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		std::cout << nums[i] << "\n";
+	}
+}
 
 /*
 Write code for function loop_vector_w_index with a vector of int pass by reference parameter.
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows : 9 10 99 5  67
 */
-
+void loop_vector_w_index_ref(std::vector<int>& nums)
+{
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		std::cout << nums[i] << "\n";
+	}
+}
 
 /*
 Write code for function loop_vector_w_index with a vector of int const pass by reference parameter.
@@ -33,3 +55,18 @@ The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows : 9 10 99 5  67
 */
 
+void loop_vector_w_index_const(const std::vector<int>& nums)
+{
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		std::cout << nums[i] << "\n";
+	}
+}
+
+void loop_vector_w_for_ranged_(const std::vector<int>& nums)
+{
+	for (auto num : nums)
+	{
+		std::cout << num << "\n";
+	}
+}
