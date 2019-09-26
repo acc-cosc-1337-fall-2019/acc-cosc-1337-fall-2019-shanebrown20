e49c9c32b7965aa7a8f53d7bae7710e045cc7264
shanebrown2090@yahoo.com <shanebrown2090@yahoo.com>
2019-09-25 22:55:27 -0500

    Finished homework

--- src/homework/03_iteration/dna.cpp
+++ src/homework/03_iteration/dna.cpp
@@ -7,37 +7,38 @@ Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
 
-/*double get_gc_content(const string& dna)
+double get_gc_content(const string& dna)
 {
-	int gc_count = 0;
-	
-	
-	for (int i = 0; i < dna.size(); ++i)
+	double count = 0;
+	int total = dna.length();
+	double gc_content;
+	for (int i = 0; i < dna.length(); ++i)
 	{
-		if (&dna[i] == "G" || &dna[i] == "C")
-			gc_count++;
+		if (dna[i] == 'G' || dna[i] == 'C')
+		{
+			count ++;
+		}
 	}
-	double gc_content = gc_count/dna.size;
+	gc_content = count/total;
 	return gc_content;
 }
-*/
+
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
 
-/*string get_reverse_string(string dna)
+string get_reverse_string(string dna)
 {
-	int k = dna.size;
-	for (int i = 0; i < dna.size(); ++i)
+	string dna_rev = "";
+	for (int i = dna.length() - 1; i >= 0; --i)
 	{
-		dna[i] = dna[k];
-		--k;
+		dna_rev += dna[i];
 	}
-	return dna;
+	return dna_rev;
 }
-*/
+
 /*
 Write prototype for function get_dna_complement that
 accepts a string dna and returns a string.
@@ -49,28 +50,27 @@ c. return string
 
 */
 
-/*string get_dna_complement(string dna)
+string get_dna_complement(string dna)
 {
 	string dna_comp = get_reverse_string(dna);
-	for (int i = 0; i < dna.size(); ++i)
+	for (int i = 0; i < dna.length(); ++i)
 	{
-		if (dna_comp[i] = "A")
+		if (dna_comp[i] == 'A')
 		{
-			dna_comp[i] = "T";
+			dna_comp[i] = 'T';
 		}
-		else if (dna_comp[i] = "T")
+		else if (dna_comp[i] == 'T')
 		{
-			dna_comp[i] = "A";
+			dna_comp[i] = 'A';
 		}
-		else if (dna_comp[i] = "C")
+		else if (dna_comp[i] == 'C')
 		{
-			dna_comp[i] = "G";
+			dna_comp[i] = 'G';
 		}
-		else if (dna_comp[i] = "G")
+		else if (dna_comp[i] == 'G')
 		{
-			dna_comp[i] = "C";
+			dna_comp[i] = 'C';
 		}
 	}
 	return dna_comp;
 }
-*/
--- src/homework/03_iteration/main.cpp
+++ src/homework/03_iteration/main.cpp
@@ -16,5 +16,37 @@ user enters a y or Y.
 */
 int main() 
 {
+	auto user_choice = 'y';
+
+	do
+	{
+		
+		
+	
+		int option;
+		string dna;
+		cout << "Enter 1 for Get GC Content or 2 for Get DNA Complement: " << "\n";
+		cin >> option;
+		cout << "Enter a DNA string: " << "\n";
+		cin >> dna;
+
+		if (option == 1)
+		{
+			cout << get_gc_content(dna) << "\n";
+		}
+		else if (option == 2)
+		{
+			cout << get_dna_complement(dna) << "\n";
+		}
+		else
+		{
+			cout << "Invalid option";
+		}
+
+
+		cout << "Loop again y or n? ";
+		cin >> user_choice;
+	} while (user_choice == 'y' || user_choice == 'Y');
+	
 	return 0;
 }
