40e174d511f9cd33f08e33ff30af529b9e9d6611
Shane Brown <shane.brown1@g.austincc.edu>
2019-11-20 19:07:31 -0600

    Assignment 11

--- src/classwork/11_assign/vector.cpp
+++ src/classwork/11_assign/vector.cpp
@@ -1,1 +1,54 @@
+#include "vector.h"
+#include <iostream>
 //
+
+Vector::Vector(size_t sz)
+	:size{sz}, nums{new int[sz]}
+{
+	for (size_t i = 0; i < sz; ++i)
+	{
+		nums[i] = 0;
+	}
+}
+Vector::Vector(const Vector& v) 
+	:size{v.size}, nums {new int[v.size]}
+{
+	for (size_t i = 0; i < size; ++i)
+	{
+		nums[i] = v[i];
+	}
+}
+Vector& Vector::operator=(const Vector& v)
+{
+	int* temp = new int[v.size];
+	for (size_t i = 0; i < v.size; ++i)
+	{
+		temp[i] = v[i];
+	}
+	delete[] nums;
+	nums = temp;
+	size = v.size;
+	return *this;
+}
+Vector::Vector(Vector&& v)
+	: size{v.size}, nums{v.nums}
+{
+	v.size = 0;
+	v.nums = nullptr;
+}
+
+Vector& Vector::operator = (Vector&& v)
+{
+	delete nums;
+	nums = v.nums;
+	size = v.size;
+	v.nums = nullptr;
+	v.size = 0;
+	return *this;
+}
+Vector::~Vector()
+{
+	delete[] nums;
+}
+
+
