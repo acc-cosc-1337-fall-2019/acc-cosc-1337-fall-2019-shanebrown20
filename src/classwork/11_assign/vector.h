19e172561bd2dd487c150e7402996fd800d657b4
Shane Brown <shane.brown1@g.austincc.edu>
2019-11-25 18:53:36 -0600

    Vector class work

--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -2,6 +2,7 @@
 class Vector
 {
 public:
+	Vector();
 	Vector(size_t sz);
 	Vector(const Vector& v); //copy constructor
 	Vector& operator=(const Vector& v); // copy assignment
@@ -10,8 +11,16 @@ public:
 	size_t Size()const { return size; }
 	int& operator[](int i) { return nums[i]; }
 	int& operator[](int i) const { return nums[i]; }
+	void Reserve(size_t new_allocation);
+	int Capacity() const { return space; }
+	void Resize(size_t new_size);
+	void Push_Back(int value);
 	~Vector();
+
 private:
-	size_t size;
+	size_t size; // space already available for array
+	size_t space; // keep track of reserve
 	int* nums;
+	const int RESERVE_DEFAULT_SIZE{ 8 };
+	const int RESERVE_SPACE_MULTIPLIER{ 2 };
 };
--- test/classwork_test/11_assign_test/vector_tests.cpp
+++ test/classwork_test/11_assign_test/vector_tests.cpp
@@ -46,3 +46,29 @@ TEST_CASE("Test return vector by value")
 
 	REQUIRE(v.Size() == 3);
 }
+
+TEST_CASE("Test reserve function new allocation")
+{
+	Vector v;
+	REQUIRE(v.Capacity() == 0);
+
+	v.Reserve(8);
+	REQUIRE(v.Capacity() == 8);
+}
+
+TEST_CASE("Test resize function")
+{
+	Vector v;
+	REQUIRE(v.Capacity() == 0);
+
+	v.Resize(10);
+	REQUIRE(v.Capacity() == 10);
+}
+TEST_CASE("Test push back with default constructor")
+{
+	Vector v;
+	REQUIRE(v.Size() == 0);
+	v.Push_Back(3);
+	REQUIRE(v.Size() == 1);
+	REQUIRE(v[0] == 3);
+}
